fdb57cbbfc9e8b73f8bb098d5d44a8006406fff9
HetalSolanki <solankih1811@gmail.com>
2024-09-06 02:35:15 +0530

    reverted

--- git_tracker/settings.py
+++ git_tracker/settings.py
@@ -87,12 +87,8 @@ WSGI_APPLICATION = "git_tracker.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "OPTIONS": {
-            "database": "git_tracker",
-            "user": "root",
-            "password": "",
-        },
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
